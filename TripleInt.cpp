bbd49a7f8f6248ac40d5f7e509c30073ba172b8b
Michael Garcia <michaelgarcia36@gmail.com>
2025-01-26 21:10:58 -0800

    Add files via upload

---
+++ TripleInt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+
+	//get input from user
+	cout << "Enter an integer: ";
+	cin >> num;
+
+	// Triple the number
+	num = num * 3;
+
+	// Output the result
+	cout << "Triple of the number is: " << num << endl;
+
+	return 0;
+}
