c61433ee47f1cabfd1ef54cb8d380dba2dd57ac6
Michael Garcia <michaelgarcia36@gmail.com>
2025-01-26 21:33:28 -0800

    Add files via upload

---
+++ SumTwoInts.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, sum;
+
+	// Prompt the user to enter the first integer
+	cout << "Enter the first integer: ";
+	cin >> num1;
+
+	// Prompt the user to enter the second integer
+	cout << "Enter the second integer: ";
+	cin >> num2;
+
+	// Calculate the sum of the two integers
+	sum = num1 + num2;
+
+	// Out the sum
+	cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+	return 0;
+
+
+}
